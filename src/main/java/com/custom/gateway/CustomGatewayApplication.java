0ff073918e738a9b9d789245700037cc750b7512
lijia <lijia@hzxituan.com>
2020-03-24 14:45:17 +0800

    开启异步功能

--- src/main/java/com/custom/gateway/CustomGatewayApplication.java
+++ src/main/java/com/custom/gateway/CustomGatewayApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @EnableRetry
 @EnableCaching
+@EnableAsync
 @MapperScan("com.custom.gateway.dao")
 @SpringBootApplication(scanBasePackages = "com.custom.gateway.*")
 @NacosPropertySource(dataId = "customer-gateway", autoRefreshed = true)
